6521375a21ab8321a72fb6a491f80092bcab9535
Roger CHARMONT <roger.charmont@orange.com>
2021-03-16 17:30:38 +0100

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/rcharmont-training/network/google"
+  version = "3.2.0"
+  network_name = "rcharmont-network"
+  project_id = var.project
+  # insert required variables here
+  subnets = [
+    {
+      subnet_name   = "rcharmont-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
